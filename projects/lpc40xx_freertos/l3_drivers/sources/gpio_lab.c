f1eb2f097d8e67aa7aa33fe18afed10d40f68a91
Akhil Cherukuri <akhilcherukuri@gmail.com>
2020-09-15 12:09:12 -0700

    Extra Credit Multiple Tasks

--- projects/lpc40xx_freertos/l3_drivers/sources/gpio_lab.c
+++ projects/lpc40xx_freertos/l3_drivers/sources/gpio_lab.c
@@ -1,21 +1,73 @@
 #include "gpio_lab.h"
 #include "lpc40xx.h"
 
-void gpiolab__set_as_input(uint8_t pin_num) { LPC_GPIO1->DIR &= ~(1U << pin_num); }
+void gpiolab__set_as_input(uint8_t pin_num, uint8_t port_num) {
+  switch (port_num) {
+  case 0:
+    LPC_GPIO0->DIR &= ~(1U << pin_num);
+    break;
+  case 1:
+    LPC_GPIO1->DIR &= ~(1U << pin_num);
+    break;
+  case 2:
+    LPC_GPIO2->DIR &= ~(1U << pin_num);
+    break;
+  default:
+    break;
+  }
+}
 
 /// Should alter the hardware registers to set the pin as output
-void gpiolab__set_as_output(uint8_t pin_num) { LPC_GPIO1->DIR |= (1U << pin_num); }
+void gpiolab__set_as_output(uint8_t pin_num, uint8_t port_num) {
+  switch (port_num) {
+  case 0:
+    LPC_GPIO0->DIR |= (1U << pin_num);
+    break;
+  case 1:
+    LPC_GPIO1->DIR |= (1U << pin_num);
+    break;
+  case 2:
+    LPC_GPIO2->DIR |= (1U << pin_num);
+    break;
+  default:
+    break;
+  }
+}
 
 /// Should alter the hardware registers to set the pin as high
-void gpiolab__set_high(uint8_t pin_num) {
-  gpiolab__set_as_output(pin_num);
-  LPC_GPIO1->SET = (1U << pin_num);
+void gpiolab__set_high(uint8_t pin_num, uint8_t port_num) {
+  gpiolab__set_as_output(pin_num, port_num);
+  switch (port_num) {
+  case 0:
+    LPC_GPIO0->SET = (1U << pin_num);
+    break;
+  case 1:
+    LPC_GPIO1->SET = (1U << pin_num);
+    break;
+  case 2:
+    LPC_GPIO2->SET = (1U << pin_num);
+    break;
+  default:
+    break;
+  }
 }
 
 /// Should alter the hardware registers to set the pin as low
-void gpiolab__set_low(uint8_t pin_num) {
-  gpiolab__set_as_output(pin_num);
-  LPC_GPIO1->CLR = (1U << pin_num);
+void gpiolab__set_low(uint8_t pin_num, uint8_t port_num) {
+  gpiolab__set_as_output(pin_num, port_num);
+  switch (port_num) {
+  case 0:
+    LPC_GPIO0->CLR = (1U << pin_num);
+    break;
+  case 1:
+    LPC_GPIO1->CLR = (1U << pin_num);
+    break;
+  case 2:
+    LPC_GPIO2->CLR = (1U << pin_num);
+    break;
+  default:
+    break;
+  }
 }
 
 /**
@@ -23,11 +75,31 @@ void gpiolab__set_low(uint8_t pin_num) {
  *
  * @param {bool} high - true => set pin high, false => set pin low
  */
-void gpiolab__set(uint8_t pin_num, bool high) {
-  if (high == true) {
-    LPC_GPIO1->SET = (1U << pin_num);
-  } else {
-    LPC_GPIO1->CLR = (1U << pin_num);
+void gpiolab__set(uint8_t pin_num, uint8_t port_num, bool high) {
+  switch (port_num) {
+  case 0:
+    if (high == true) {
+      LPC_GPIO0->SET = (1U << pin_num);
+    } else {
+      LPC_GPIO0->CLR = (1U << pin_num);
+    }
+    break;
+  case 1:
+    if (high == true) {
+      LPC_GPIO1->SET = (1U << pin_num);
+    } else {
+      LPC_GPIO1->CLR = (1U << pin_num);
+    }
+    break;
+  case 2:
+    if (high == true) {
+      LPC_GPIO2->SET = (1U << pin_num);
+    } else {
+      LPC_GPIO2->CLR = (1U << pin_num);
+    }
+    break;
+  default:
+    break;
   }
 }
 
@@ -36,9 +108,20 @@ void gpiolab__set(uint8_t pin_num, bool high) {
  *
  * @return {bool} level of pin high => true, low => false
  */
-bool gpiolab__get_level(uint8_t pin_num) {
+bool gpiolab__get_level(uint8_t pin_num, uint8_t port_num) {
   bool level = false;
-  level = (bool)(LPC_GPIO1->PIN & (1U << pin_num));
-
+  switch (port_num) {
+  case 0:
+    level = (bool)(LPC_GPIO0->PIN & (1U << pin_num));
+    break;
+  case 1:
+    level = (bool)(LPC_GPIO1->PIN & (1U << pin_num));
+    break;
+  case 2:
+    level = (bool)(LPC_GPIO2->PIN & (1U << pin_num));
+    break;
+  default:
+    break;
+  }
   return level;
 }
