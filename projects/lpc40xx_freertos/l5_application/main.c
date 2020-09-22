91986673f222f770a9baafa4001bfb10e6c22be0
Akhil Cherukuri <akhilcherukuri@gmail.com>
2020-09-21 23:25:26 -0700

    Part 1

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -10,7 +10,46 @@
 #include "lpc_peripherals.h"
 #include "sj2_cli.h"
 
-#define Part_0
+#define Part_1
+
+#ifdef Part_1
+static SemaphoreHandle_t switch_pressed_signal;
+void gpio_interrupt(void);
+void sleep_on_sem_task(void *p);
+void configure_your_gpio_interrupt(void);
+void clear_gpio_interrupt(void);
+
+static gpio_s sw2 = {0, 30};
+static gpio_s led_sw2 = {1, 24};
+
+void configure_your_gpio_interrupt() {
+  gpio__set_as_input(sw2);
+  gpio__set_as_output(led_sw2);
+  gpio__enable_pull_down_resistors(sw2);
+
+  LPC_GPIOINT->IO0IntEnF |= (1 << 30);
+}
+
+void clear_gpio_interrupt() { LPC_GPIOINT->IO0IntClr |= (1 << 30); }
+
+void gpio_interrupt(void) {
+  fprintf(stderr, "ISR Entry \n");
+  xSemaphoreGiveFromISR(switch_pressed_signal, NULL);
+  clear_gpio_interrupt();
+  fprintf(stderr, "ISR Exit \n");
+}
+
+void sleep_on_sem_task(void *p) {
+  while (1) {
+    if (xSemaphoreTake(switch_pressed_signal, portMAX_DELAY)) {
+      delay__ms(250);
+      gpio__set(led_sw2);
+      delay__ms(250);
+      gpio__reset(led_sw2);
+    }
+  }
+}
+#endif
 
 #ifdef Part_0
 void gpio_interrupt(void) {
@@ -39,6 +78,17 @@ int main(void) {
     delay__ms(250);
     gpio__reset(led_sw2);
   }
+#endif
+
+#ifdef Part_1
+  switch_pressed_signal = xSemaphoreCreateBinary();
+  lpc_peripheral__enable_interrupt(LPC_PERIPHERAL__GPIO, gpio_interrupt, "gpio_interrupt");
+
+  configure_your_gpio_interrupt();
+  NVIC_EnableIRQ(GPIO_IRQn);
+
+  xTaskCreate(sleep_on_sem_task, "sem", (512U * 4) / sizeof(void *), NULL, PRIORITY_LOW, NULL);
+  vTaskStartScheduler();
 #endif
 
   return 0;
