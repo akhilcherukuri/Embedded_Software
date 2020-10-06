e50cff62b9e529cd74ecc60e428ff23e18e12863
Akhil Cherukuri <akhilcherukuri@gmail.com>
2020-10-06 11:24:15 -0700

    Part 2 Reworked

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -25,7 +25,8 @@ void ssp2_pin_configuration(void);
 void spi_task(void *p);
 #endif
 #ifdef Part_2
-void spi_id_verification_task(void *p);
+void spi_id_verification_task1(void *p);
+void spi_id_verification_task1(void *p);
 SemaphoreHandle_t spi_flash_mutex;
 #endif
 static adesto_flash_id_s adesto_read_signature(void);
@@ -89,7 +90,7 @@ void spi_task(void *p) {
 #endif
 
 #ifdef Part_2
-void spi_id_verification_task(void *p) {
+void spi_id_verification_task1(void *p) {
   const uint32_t spi_clock_mhz = 24;
   ssp2__init(spi_clock_mhz);
   ssp2_pin_configuration();
@@ -98,6 +99,32 @@ void spi_id_verification_task(void *p) {
     if (xSemaphoreTake(spi_flash_mutex, 1000)) {
       const adesto_flash_id_s id = adesto_read_signature();
       if (id.manufacturer_id == 0x1F) {
+        printf("Task 1| ");
+        printf("Manufacturer ID %x| ", id.manufacturer_id);
+        printf("Device ID1 %x| ", id.device_id_1);
+        printf("Device ID2 %x| ", id.device_id_2);
+        printf("Ext Device ID %x | \n", id.extended_device_id);
+      }
+      // When we read a manufacturer ID we do not expect, we will kill this task
+      if (id.manufacturer_id != 0x1F) {
+        fprintf(stderr, "Manufacturer ID read failure\n");
+        vTaskSuspend(NULL); // Kill this task
+      }
+      xSemaphoreGive(spi_flash_mutex);
+    }
+    vTaskDelay(500);
+  }
+}
+void spi_id_verification_task2(void *p) {
+  const uint32_t spi_clock_mhz = 24;
+  ssp2__init(spi_clock_mhz);
+  ssp2_pin_configuration();
+  gpio__set_as_output(adesto_external_flash_cs_signal);
+  while (1) {
+    if (xSemaphoreTake(spi_flash_mutex, 1000)) {
+      const adesto_flash_id_s id = adesto_read_signature();
+      if (id.manufacturer_id == 0x1F) {
+        printf("Task 2| ");
         printf("Manufacturer ID %x| ", id.manufacturer_id);
         printf("Device ID1 %x| ", id.device_id_1);
         printf("Device ID2 %x| ", id.device_id_2);
@@ -118,8 +145,8 @@ void spi_id_verification_task(void *p) {
 int main(void) {
 #ifdef Part_2
   spi_flash_mutex = xSemaphoreCreateMutex();
-  xTaskCreate(spi_id_verification_task, "SPI ID Task 1", 4096, NULL, PRIORITY_LOW, NULL);
-  xTaskCreate(spi_id_verification_task, "SPI ID Task 2", 4096, NULL, PRIORITY_LOW, NULL);
+  xTaskCreate(spi_id_verification_task1, "SPI ID Task 1", 4096, NULL, PRIORITY_LOW, NULL);
+  xTaskCreate(spi_id_verification_task2, "SPI ID Task 2", 4096, NULL, PRIORITY_LOW, NULL);
 #endif
 #ifdef Part_1
   xTaskCreate(spi_task, "SPI Part 1", 4096 / sizeof(void *), NULL, PRIORITY_LOW, NULL);
