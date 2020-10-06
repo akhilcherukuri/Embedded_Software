a6c70656c42881eafd9d027f25420c8618f4be12
Akhil Cherukuri <akhilcherukuri@gmail.com>
2020-10-05 23:41:54 -0700

    Part 0

---
+++ projects/lpc40xx_freertos/l5_application/ssp2_lab.c
@@ -0,0 +1,27 @@
+#include "ssp2_lab.h"
+#include "gpio.h"
+#include "lpc40xx.h"
+
+void ssp2__init(uint32_t max_clock_mhz) {
+  // Refer to LPC User manual and setup the register bits correctly
+  // a) Power on Peripheral
+  const uint32_t power_on_bit = (1 << 20);
+  LPC_SC->PCONP |= power_on_bit;
+  // b) Setup control registers CR0 and CR1
+  const uint32_t dss_bits = (0b111 << 0);
+  const uint32_t sse_bit = (0b1 << 0);
+  LPC_SSP2->CR0 |= dss_bits;
+  LPC_SSP2->CR1 |= sse_bit;
+  // c) Setup prescalar register to be <= max_clock_mhz
+  LPC_SSP2->CPSR = 4;
+}
+
+uint8_t ssp2__exchange_byte_lab(uint8_t data_out) {
+  // Configure the Data register(DR) to send and receive data by checking the SPI peripheral status register
+  const uint32_t busy_bit = (1 << 4);
+  LPC_SSP2->DR = data_out;
+  while (LPC_SSP2->SR & busy_bit) {
+    ;
+  }
+  return LPC_SSP2->DR;
+}
