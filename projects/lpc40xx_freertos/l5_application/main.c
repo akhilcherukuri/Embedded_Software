886f178932e0cbb36a8241099254e4e4392bcbf0
Akhil Cherukuri <akhilcherukuri@gmail.com>
2020-10-19 23:39:05 -0700

    Lab with PDF

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -1,105 +1,85 @@
+/*
+Explanation and Screenshot are in PDF in given Merge Request.
+
+Additional Questions:
+Q1) What is the purpose of the block time during xQueueReceive()?
+A1) The max amount of time(in ticks) to wait for queue to receive data.
+
+Q2) What if you use ZERO block time during xQueueReceive()?
+A2) If no data is received in queue by function call, continues forward to task completion.
+*/
+
 #include <stdio.h>
 
 #include "FreeRTOS.h"
+#include "lpc40xx.h"
+#include "queue.h"
 #include "task.h"
 
-#include "board_io.h"
-#include "common_macros.h"
-#include "periodic_scheduler.h"
 #include "sj2_cli.h"
 
-// 'static' to make these functions 'private' to this file
-static void create_blinky_tasks(void);
-static void create_uart_task(void);
-static void blink_task(void *params);
-static void uart_task(void *params);
+static QueueHandle_t switch_queue;
+
+typedef enum { switch__off, switch__on } switch_e;
+
+void producer(void *p);
+void consumer(void *p);
+switch_e get_switch_input_from_switch0(void);
 
 int main(void) {
-  create_blinky_tasks();
-  create_uart_task();
-
-  // If you have the ESP32 wifi module soldered on the board, you can try uncommenting this code
-  // See esp32/README.md for more details
-  // uart3_init();                                                                     // Also include:  uart3_init.h
-  // xTaskCreate(esp32_tcp_hello_world_task, "uart3", 1000, NULL, PRIORITY_LOW, NULL); // Include esp32_task.h
 
   puts("Starting RTOS");
+  // TODO: Create your tasks
+  xTaskCreate(producer, "producer", 4096, NULL, PRIORITY_LOW, NULL);
+  xTaskCreate(consumer, "consumer", 4096, NULL, PRIORITY_LOW, NULL);
+
+  // TODO Queue handle is not valid until you create it
+  switch_queue =
+      xQueueCreate(1, sizeof(switch_e)); // Choose depth of item being our enum (1 should be okay for this example)
+
   vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
 
   return 0;
 }
 
-static void create_blinky_tasks(void) {
-  /**
-   * Use '#if (1)' if you wish to observe how two tasks can blink LEDs
-   * Use '#if (0)' if you wish to use the 'periodic_scheduler.h' that will spawn 4 periodic tasks, one for each LED
-   */
-#if (1)
-  // These variables should not go out of scope because the 'blink_task' will reference this memory
-  static gpio_s led0, led1;
+// TODO: Create this task at PRIORITY_LOW
+void producer(void *p) {
+  while (1) {
+    // This xQueueSend() will internally switch context to "consumer" task because it is higher priority than this
+    // "producer" task Then, when the consumer task sleeps, we will resume out of xQueueSend()and go over to the next
+    // line
 
-  led0 = board_io__get_led0();
-  led1 = board_io__get_led1();
+    // TODO: Get some input value from your board
+    const switch_e switch_value = get_switch_input_from_switch0();
 
-  xTaskCreate(blink_task, "led0", configMINIMAL_STACK_SIZE, (void *)&led0, PRIORITY_LOW, NULL);
-  xTaskCreate(blink_task, "led1", configMINIMAL_STACK_SIZE, (void *)&led1, PRIORITY_LOW, NULL);
-#else
-  const bool run_1000hz = true;
-  const size_t stack_size_bytes = 2048 / sizeof(void *); // RTOS stack size is in terms of 32-bits for ARM M4 32-bit CPU
-  periodic_scheduler__initialize(stack_size_bytes, !run_1000hz); // Assuming we do not need the high rate 1000Hz task
-  UNUSED(blink_task);
-#endif
-}
-
-static void create_uart_task(void) {
-  // It is advised to either run the uart_task, or the SJ2 command-line (CLI), but not both
-  // Change '#if (0)' to '#if (1)' and vice versa to try it out
-#if (0)
-  // printf() takes more stack space, size this tasks' stack higher
-  xTaskCreate(uart_task, "uart", (512U * 8) / sizeof(void *), NULL, PRIORITY_LOW, NULL);
-#else
-  sj2_cli__init();
-  UNUSED(uart_task); // uart_task is un-used in if we are doing cli init()
-#endif
-}
-
-static void blink_task(void *params) {
-  const gpio_s led = *((gpio_s *)params); // Parameter was input while calling xTaskCreate()
-
-  // Warning: This task starts with very minimal stack, so do not use printf() API here to avoid stack overflow
-  while (true) {
-    gpio__toggle(led);
-    vTaskDelay(500);
+    // TODO: Print a message before xQueueSend()
+    printf("Producer Task \n");
+    // Note: Use printf() and not fprintf(stderr, ...) because stderr is a polling printf
+    xQueueSend(switch_queue, &switch_value, 0);
+    // TODO: Print a message after xQueueSend()
+    printf("Line %d, sending switch status: %i \n", __LINE__, switch_value);
+    vTaskDelay(1000);
   }
 }
 
-// This sends periodic messages over printf() which uses system_calls.c to send them to UART0
-static void uart_task(void *params) {
-  TickType_t previous_tick = 0;
-  TickType_t ticks = 0;
+// TODO: Create this task at PRIORITY_HIGH
+void consumer(void *p) {
+  switch_e switch_value;
+  while (1) {
+    // TODO: Print a message before xQueueReceive()
+    printf("Cosumer Task \n");
+    xQueueReceive(switch_queue, &switch_value, portMAX_DELAY);
+    // TODO: Print a message after xQueueReceive()
+    printf("Line %d, receving switch status: %i \n", __LINE__, switch_value);
+  }
+}
 
-  while (true) {
-    // This loop will repeat at precise task delay, even if the logic below takes variable amount of ticks
-    vTaskDelayUntil(&previous_tick, 2000);
-
-    /* Calls to fprintf(stderr, ...) uses polled UART driver, so this entire output will be fully
-     * sent out before this function returns. See system_calls.c for actual implementation.
-     *
-     * Use this style print for:
-     *  - Interrupts because you cannot use printf() inside an ISR
-     *    This is because regular printf() leads down to xQueueSend() that might block
-     *    but you cannot block inside an ISR hence the system might crash
-     *  - During debugging in case system crashes before all output of printf() is sent
-     */
-    ticks = xTaskGetTickCount();
-    fprintf(stderr, "%u: This is a polled version of printf used for debugging ... finished in", (unsigned)ticks);
-    fprintf(stderr, " %lu ticks\n", (xTaskGetTickCount() - ticks));
-
-    /* This deposits data to an outgoing queue and doesn't block the CPU
-     * Data will be sent later, but this function would return earlier
-     */
-    ticks = xTaskGetTickCount();
-    printf("This is a more efficient printf ... finished in");
-    printf(" %lu ticks\n\n", (xTaskGetTickCount() - ticks));
+switch_e get_switch_input_from_switch0(void) {
+  uint32_t switch0 = (1 << 29);
+  LPC_GPIO0->DIR &= ~(1 << 29);
+  if (LPC_GPIO0->PIN & switch0) {
+    return switch__on;
+  } else {
+    return switch__off;
   }
 }
