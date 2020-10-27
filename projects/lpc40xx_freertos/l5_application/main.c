4ebb6ce9b7d7b3e677d8b1d8a05b127f7815fed6
Akhil Cherukuri <akhilcherukuri@gmail.com>
2020-10-26 17:54:43 -0700

    Revised:Completed Group Assignment

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -18,6 +18,7 @@
 #include "FreeRTOS.h"
 #include "event_groups.h"
 #include "queue.h"
+#include "semphr.h"
 #include "task.h"
 
 #include "acceleration.h"
@@ -28,6 +29,7 @@
 /* Handles */
 static QueueHandle_t sensor_queue;
 static EventGroupHandle_t checkin;
+static SemaphoreHandle_t mutex;
 
 /* Tasks */
 static void producer_task(void *p);
@@ -36,6 +38,7 @@ static void watchdog_task(void *p);
 
 /* Helper functions */
 static void write_file_using_fatfs_pi(int16_t value, const char *filename);
+static void write_error_using_fatfs_pi(char error_message[], const char *filename);
 
 /* Static global variables */
 static const EventBits_t bit_1 = (1 << 1);
@@ -45,6 +48,7 @@ int main(void) {
 
   sensor_queue = xQueueCreate(100, sizeof(int16_t));
   checkin = xEventGroupCreate();
+  mutex = xSemaphoreCreateMutex();
   acceleration__init();
 
   xTaskCreate(producer_task, "producer", 2048 / sizeof(void *), NULL, PRIORITY_MEDIUM, NULL);
@@ -62,18 +66,18 @@ static void producer_task(void *p) {
   int16_t average_sensor_value = 0;
   int16_t sensor_values_sum = 0;
   size_t sensor_values_count = 0;
-  acceleration__axis_data_s accelerometer_axis_data = {0};
 
   while (1) {
     if (sensor_values_count < 100) {
-      accelerometer_axis_data = acceleration__get_data();
-      sensor_values_sum += accelerometer_axis_data.x;
+      sensor_values_sum += acceleration__get_data().x;
       sensor_values_count++;
       vTaskDelay(1);
     } else {
       average_sensor_value = (sensor_values_sum / sensor_values_count);
       xQueueSend(sensor_queue, &average_sensor_value, 0);
       xEventGroupSetBits(checkin, bit_1);
+      sensor_values_sum = 0;
+      sensor_values_count = 0;
       vTaskDelay(100);
     }
   }
@@ -85,19 +89,22 @@ static void consumer_task(void *p) {
   const char *filename = "accelerometer_x_axis_data.csv";
 
   while (1) {
-    if (xQueueReceive(sensor_queue, &average_sensor_value, portMAX_DELAY)) {
-      xEventGroupSetBits(checkin, bit_2);
-      // Open a file and append the data to an output file on the SD card every 1 sec or so
-      if (sys_time__get_uptime_ms() - time_elapsed > 1000) {
+    xQueueReceive(sensor_queue, &average_sensor_value, portMAX_DELAY);
+    // Open a file and append the data to an output file on the SD card every 1 sec or so
+    if (sys_time__get_uptime_ms() - time_elapsed > 1000) {
+      if (xSemaphoreTake(mutex, 10) == pdTRUE) {
         write_file_using_fatfs_pi(average_sensor_value, filename);
-        time_elapsed = sys_time__get_uptime_ms();
+        xSemaphoreGive(mutex);
       }
+      time_elapsed = sys_time__get_uptime_ms();
     }
+    xEventGroupSetBits(checkin, bit_2);
   }
 }
 
 static void watchdog_task(void *p) {
   const EventBits_t wait_for_bit_1_bit_2 = bit_1 | bit_2;
+  const char *filename = "accelerometer_x_axis_data.csv";
   EventBits_t bits_set;
 
   while (1) {
@@ -108,8 +115,24 @@ static void watchdog_task(void *p) {
     } else if ((bits_set & bit_1) != 0) {
       printf("Check-in successfull from producer task\n");
       printf("Consumer task failed to check-in\n");
+      if (xSemaphoreTake(mutex, 10) == pdTRUE) {
+        write_error_using_fatfs_pi("ERROR: Consumer task failed to check-in", filename);
+        xSemaphoreGive(mutex);
+      }
+    } else if ((bits_set & bit_2) != 0) {
+      printf("Check-in successfull from consumer task\n");
+      printf("Producer task failed to check-in\n");
+      if (xSemaphoreTake(mutex, 10) == pdTRUE) {
+        write_error_using_fatfs_pi("ERROR: Producer task failed to check-in", filename);
+        xSemaphoreGive(mutex);
+      }
     } else {
-      printf("Producer and consumer task failed to check-in within the 200ms threshold\n");
+      printf("Producer and Consumer task failed to check-in within the 200ms threshold\n");
+      if (xSemaphoreTake(mutex, 10) == pdTRUE) {
+        write_error_using_fatfs_pi("ERROR: Producer and Consumer task failed to check-in within the 200ms threshold",
+                                   filename);
+        xSemaphoreGive(mutex);
+      }
     }
     vTaskDelay(1000);
   }
@@ -124,6 +147,24 @@ static void write_file_using_fatfs_pi(int16_t value, const char *filename) {
     char string[64];
     sprintf(string, "%li, %i\n", xTaskGetTickCount(), value);
     if (FR_OK == f_write(&file, string, strlen(string), &bytes_written)) {
+    } else {
+      printf("ERROR: Failed to write data to file\n");
+    }
+    f_close(&file);
+  } else {
+    printf("ERROR: Failed to open: %s\n", filename);
+  }
+}
+
+static void write_error_using_fatfs_pi(char error_message[], const char *filename) {
+  FIL file; // File handle
+  UINT bytes_written = 0;
+  FRESULT result = f_open(&file, filename, (FA_WRITE | FA_OPEN_APPEND));
+
+  if (FR_OK == result) {
+    char error_string[128];
+    sprintf(error_string, "%li, %s \n", xTaskGetTickCount(), error_message);
+    if (FR_OK == f_write(&file, error_string, strlen(error_string), &bytes_written)) {
     } else {
       printf("ERROR: Failed to write data to file\n");
     }
